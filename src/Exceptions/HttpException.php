1afb2f7f8bbc4cfbbdaa897dd7177a75736b2276
ossycodes <ossyfizy009@gmail.com>
2023-09-20 16:45:56 +0100

    fix namespaces

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ossycodes\Nigeriabulksms;
+namespace Ossycodes\Nigeriabulksms\Exceptions;
 
 /**
  * Class HttpException
---
+++ src/Exceptions/RequestException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ossycodes\Nigeriabulksms\Exceptions;
+
+/**
+ * Class RequestException
+ */
+class RequestException extends NigeriaBulksmsException
+{
+
+}
