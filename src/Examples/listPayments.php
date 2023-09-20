45744e5a3b46211b097df4d456e5c2dfa9c82931
ossycodes <ossyfizy009@gmail.com>
2023-09-21 00:13:03 +0100

    add listPayments example

---
+++ src/Examples/listPayments.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once(__DIR__ . '/../autoload.php');
+
+$config = \Ossycodes\Nigeriabulksms\Configuration::getDefaultConfiguration()
+            ->setUsername('YOUR_USERNAME')
+            ->setPassword('YOUR_PASSWORD')
+            ->setTimeout(10) //optional defaults to 10
+            ->setConnectionTimeout(2); //optional defaults to 2
+
+$client = new \Ossycodes\Nigeriabulksms\Client($config);
+
+try {
+    $payments = $client->payments->getList();
+    var_dump($balance);
+
+    //iterate through each payment item
+    foreach($client->payments->getList()->getItems() as $payment) {
+        dump($payment->amount);
+        dump($payment->reference);
+        dump($payment->date);
+    }
+
+} catch (\Ossycodes\Nigeriabulksms\Exceptions\AuthenticateException $e) {
+    // That means that your username and/or password is incorrect
+    echo 'wrong login';
+}
+catch (\Ossycodes\Nigeriabulksms\Exceptions\BalanceException $e) {
+    // That means that your balance is insufficient
+    echo 'insufficient balance';
+}
+catch (\Exception $e) {
+    var_dump($e->getMessage());
+}
