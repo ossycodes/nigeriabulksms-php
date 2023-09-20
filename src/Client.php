1865171ec3f7cf90c4fbe20933a1c1baacdd08db
ossycodes <ossyfizy009@gmail.com>
2023-09-20 15:35:52 +0100

    set client, add balance action wip

---
+++ src/Client.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Ossycodes\Nigeriabulksms;
+
+use Ossycodes\Nigeriabulksms\Configuration;
+use Ossycodes\Nigeriabulksms\Actions\Balance;
+use Ossycodes\Nigeriabulksms\Common\HttpClient;
+
+class Client
+{
+    public const ENDPOINT = 'https://portal.nigeriabulksms.com/api/';
+
+    public const CLIENT_VERSION = 1.0;
+
+    /**
+     * @var Configuration
+     */
+    public $config;
+
+    /**
+     * @var HttpClient
+     */
+    public $httpClient;
+
+    /**
+     * @var Balance
+     */
+    public $balance;
+
+    public function __construct(Configuration $config, ?HttpClient $httpClient = null)
+    {
+        $this->config = $config;
+
+        if ($this->config === null) {
+            throw new AuthenticateException('Cannot perform API Request without username and password set as the default configuration');
+        }
+
+        if ($this->config->getUsername() === null) {
+            throw new AuthenticateException('Cannot perform API Request without username set');
+        }
+
+        if ($this->config->getPassword() === null) {
+            throw new AuthenticateException('Cannot perform API Request without password set');
+        }
+
+        $this->httpClient = new HttpClient(self::ENDPOINT, $this->config);
+
+        $this->httpClient->addUserAgentString('Ossycodes/Nigeriabulksms/ApiClient/' . self::CLIENT_VERSION);
+        $this->httpClient->addUserAgentString($this->getPhpVersion());
+
+        $this->balance = new Balance($this->httpClient);
+
+    }
+
+    private function getPhpVersion(): string
+    {
+        if (!\defined('PHP_VERSION_ID')) {
+            $version = array_map('intval', explode('.', \PHP_VERSION));
+            \define('PHP_VERSION_ID', $version[0] * 10000 + $version[1] * 100 + $version[2]);
+        }
+
+        return 'PHP/' . \PHP_VERSION_ID;
+    }
+}
